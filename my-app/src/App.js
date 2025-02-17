bb8b54615ca4f30b73857c01e84c4ae1396f42d4
LucasGoulart24 <Goulart7720@gmail.com>
2025-02-17 14:39:50 -0300

    app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,9 +7,18 @@ function App() {
       <nav className="Menu">
         <div className="Logo">
           <img src={logo} alt="logo"></img>
-          <h3>Sweet Ice Screen</h3>
+          <h3>Sweet Ice Scream</h3>
         </div>
       </nav>
+      <div className="Banner">
+        <h1>Sweet Ice Scream</h1>
+      </div>
+      <div className="Titulo">
+        <h2>Nossos Sabores</h2>
+      </div>
+      <div className="Copyright">
+        <h3>&copy;2025 - Sweet Ice Scream</h3>
+      </div>
     </div>
   );
 }
